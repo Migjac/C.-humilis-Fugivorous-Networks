4be2f140316478ee18cc9689dda066afdb514c76
Migjac <mjacome@ebd.csic.es>
2017-04-27 16:37:14 +0200

    analysis with efficiency no unknowns

---
+++ Frugivore Efficiency/Efficiency_without_unk.R
@@ -0,0 +1,32 @@
+#Martinazo
+mart_enun<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/Dispersal-Networks/Efficiency/mart_eff_nun.csv",header=TRUE,check.names=FALSE,row.names=1)
+
+#Matasgordas
+matas_enun<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/Dispersal-Networks/Efficiency/matas_eff_nun.csv",header=TRUE,check.names=FALSE,row.names=1)
+
+#Martinazo Bipartite network
+par(mfrow = c(2,1))
+plotweb(mart_enun, col.low=c(gray(seq(0.1,1,length=22))), col.high=c("white","white","gray30","gray30","black","darkgray"),method="normal", text.rot=90,low.lablength=10, high.lablength=8)
+
+#Matasgordas Bipartite Network
+plotweb(matas_enun, col.low=c(gray(seq(0.1,1,length=34))), col.high=c("white","white","gray30","gray30","black","black","darkgray"), method="normal", text.rot=90,low.lablength=10, high.lablength=8)
+
+#networklevel
+
+#Martinazo
+networklevel(mart_enun, index = c("connectance","web asymmetry","cluster coefficient","nestedness","weighted nestedness"))
+
+#Matasgordas
+networklevel(matas_enun,index = c("connectance","web asymmetry","cluster coefficient","nestedness","weighted nestedness"))
+
+#Modularity
+mod_mart_enun<-computeModules(mart_enun)
+mod_matas_enun<-computeModules(matas_enun)
+
+
+
+mod_mart_enun@likelihood
+mod_matas_enun@likelihood
+
+linklevel(mart_enun)
+linklevel(matas_enun)
--- Interaction Motifs/Interaction Motifs.R
+++ Interaction Motifs/Interaction Motifs.R
@@ -12,3 +12,27 @@ martmat_eNA
 median(as.matrix(martmat_eNA),na.rm=TRUE)
 
 mart_e<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/Dispersal-Networks/Efficiency/mart_eff.csv",header=TRUE,check.names=FALSE,row.names=1)
+
+#Martinazo
+mart_mat<-read.csv("~/Desktop/red_miguel.csv",header=TRUE,check.names=FALSE,row.names=1)
+
+#Matasgordas
+matas_e<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/Dispersal-Networks/Efficiency/matas_eff.csv",header=TRUE,check.names=FALSE,row.names=1)
+
+
+library(reshape2)
+setNames(melt(mart_mat), c('plant', 'deer', 'values'))
+#  rows vars values
+mart_melt<-melt(mart_e,id)
+
+mart_melt
+
+####Analyzing the interaction motifs with different characteristics of the plants
+
+inter_mot<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/C.-humilis-Fugivorous-Networks/Interaction Motifs/red_miguel.csv",header=TRUE,check.names=FALSE,row.names=1)
+mod_intmot<-glm(inter_mot$nn~inter_mot$Motifs)
+anova(mod_intmot)
+summary(mod_intmot)
+boxplot(inter_mot$dens,inter_mot$Motifs)
+
+
