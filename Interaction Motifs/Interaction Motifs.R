b4abff218731d0652dddf29fc63f08d81894c880
Migjac <mjacome@ebd.csic.es>
2017-05-04 11:41:55 +0200

    deleted some analysis

--- Interaction Motifs/Interaction Motifs.R
+++ Interaction Motifs/Interaction Motifs.R
@@ -20,19 +20,6 @@ mart_mat<-read.csv("~/Desktop/red_miguel.csv",header=TRUE,check.names=FALSE,row.
 matas_e<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/Dispersal-Networks/Efficiency/matas_eff.csv",header=TRUE,check.names=FALSE,row.names=1)
 
 
-library(reshape2)
-setNames(melt(mart_mat), c('plant', 'deer', 'values'))
-#  rows vars values
-mart_melt<-melt(mart_e,id)
-
-mart_melt
-
-####Analyzing the interaction motifs with different characteristics of the plants
-
-inter_mot<-read.csv("~/Desktop/Palmito/Postdoc-Interaction Networks/C.-humilis-Fugivorous-Networks/Interaction Motifs/red_miguel.csv",header=TRUE,check.names=FALSE,row.names=1)
-mod_intmot<-glm(inter_mot$nn~inter_mot$Motifs)
-anova(mod_intmot)
-summary(mod_intmot)
-boxplot(inter_mot$dens,inter_mot$Motifs)
+summary(interaction_motifs)
 
 
